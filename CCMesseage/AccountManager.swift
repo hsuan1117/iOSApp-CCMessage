41df0019820c012992a87eb99a6a70d990a07b71
Hsuan <hsuan.dev@gmail.com>
2020-05-22 09:05:30 +0800

    0522 first

--- CCMesseage/AccountManager.swift
+++ CCMesseage/AccountManager.swift
@@ -10,16 +10,22 @@ import Foundation
 import Firebase
 import SwifterSwift
 
+struct AuthResult {
+    var auth : AuthDataResult?
+    var error : Error?
+}
+
 class Account {
-    func registerWithPassword(account:String,password:String){
+    func registerWithPassword(account:String,password:String,completion:(AuthResult)->()){
         Auth.auth().createUser(withEmail: account, password: password) { authResult, error in
-            
+            completion(AuthResult(auth: authResult,error: error))
         }
     }
-    func loginWithPassword(account:String,password:String){
+    func loginWithPassword(account:String,password:String,completion:(AuthResult)->()){
         Auth.auth().signIn(withEmail: account, password: password) { [weak self] authResult, error in
             guard let strongSelf = self else { return }
             
+            completion(AuthResult(auth: authResult, error: error))
         }
     }
     init(){
--- CCMesseage/AccountView.swift
+++ CCMesseage/AccountView.swift
@@ -1,17 +1,9 @@
-//
-//  AccountView.swift
-//  CCMesseage
-//
-//  Created by student on 2020/4/17.
-//  Copyright © 2020 hsuan. All rights reserved.
-//
-
 import UIKit
 import FirebaseAuth
 import SwifterSwift
 
 class AccountView: UIViewController {
-    let fAccount = Account()
+    let ACM = Account()
     
     @IBOutlet weak var inputPassword: UITextField!
     @IBOutlet weak var inputAccount: UITextField!
@@ -25,25 +17,29 @@ class AccountView: UIViewController {
         }else if inputPassword.isEmpty {
             showAlert(title: "警告", message: "密碼為空")
         }else{
-            fAccount.registerWithPassword(account: inputPassword.text!, password: inputAccount.text!)
+            ACM.registerWithPassword(account: inputAccount.text!, password: inputPassword.text!, completion: {
+                result in
+                
+            })
         }
     }
     @IBAction func OnclickLogin(_ sender: Any) {
-       
         if inputAccount.isEmpty {
             showAlert(title: "警告", message: "帳號為空")
         }else if inputPassword.isEmpty {
             showAlert(title: "警告", message: "密碼為空")
         }else{
+            
             fAccount.loginWithPassword(account: inputPassword.text!, password: inputAccount.text!)
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let pLayer = UIView(frame: CGRect(x: 0, y: 0, width: 1000, height: 1000))
         pLayer.backgroundColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
         self.view.addSubview(pLayer)
-        //self.view.bringSubviewToFront(pLayer)
+        
         
         Auth.auth().addStateDidChangeListener { (auth, user) in
             if user != nil {
@@ -61,18 +57,5 @@ class AccountView: UIViewController {
                 
             }
         }
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
